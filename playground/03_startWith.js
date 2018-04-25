2cee04b2656015ab51235de95b48fdad18577159
kotar0 <koutaro.okuya@gmail.com>
2018-04-26 01:44:40 +0900

    内容追加

--- playground/03_startWith.js
+++ playground/03_startWith.js
@@ -5,9 +5,32 @@ const initialState = 100;
 const stream$ = Rx.Observable.of(2,3,4)
                     .map(x => x*100)
                     .startWith(initialState)
-                    .scan((x, y) => {
+                    .reduce((x, y) => {
                       return x + y
                     })
                     .subscribe(
                       succ => console.log(succ)
                     )
+
+/*
+
+startWithは初期値のセットに使う。
+
+scanとreduceの違い。
+scan : 毎回の実行結果をストリームに流す
+reduce : 最終まで実行して、その結果を返す
+
+scan()
+​​​​​100​​​​​
+
+​​​​​300​​​​​
+
+​​​​​600​​​​​
+
+​​​​​1000​​​​​
+
+
+reduce()
+1000
+
+ */
