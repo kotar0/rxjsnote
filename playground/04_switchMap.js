8a66fd7c79b4ee87f1317dea72fd5e939717c548
kotar0 <koutaro.okuya@gmail.com>
2018-04-28 09:01:21 +0900

    mergeMap追加

--- playground/04_switchMap.js
+++ playground/04_switchMap.js
@@ -1,12 +1,14 @@
-const Rx = require('rxjs');
+const Rx = require("rxjs");
 
 const initialState = 100;
 
-const stream$ = Rx.Observable.of(2,3,4)
-                    .switchMap(n => Rx.Observable.timer(n*1000).mapTo(n))
-                    .subscribe(
-                      succ => console.log(succ)
-                    )
+const stream$ = Rx.Observable.of(2, 3, 4)
+  .switchMap(n => Rx.Observable.timer(n * 1000).mapTo(n))
+  .subscribe(succ => console.log(succ));
+
+const stream2$ = Rx.Observable.of(2, 3, 4)
+  .mergeMap(n => Rx.Observable.timer(n * 1000).mapTo(n))
+  .subscribe(succ => console.log(succ));
 
 /*
 switchMap, mergeMap, 
